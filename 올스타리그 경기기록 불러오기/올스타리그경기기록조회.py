80bb3d7164abbcb2d889a10e1c1ba8c4e9fadffd
xldyxl <xldyxl@gmail.com>
2026-05-29 03:01:32 +0900

    Optimize: 03:01:32

--- 올스타리그 경기기록 불러오기/올스타리그경기기록조회.py
+++ 올스타리그 경기기록 불러오기/올스타리그경기기록조회.py
@@ -7,13 +7,15 @@ api_key = "live_f4551344020f5d8e5aa2e29c8fea68b1cc76b722b41becd6437c639aef430e52
 headers = {"x-nxopen-api-key": api_key}
 
 league_players = [
-    "Voor", "UNIT", "아들러", "ZD장인지참치", "검정소", 
-    "Angel코레아", "0708ManUtd", "chilishake", "Special블루", "Gucci박장군", 
-    "ZD장인흥미니7", "조바리안", "kingdom21", "오스트리아", "은안", "FC올로냐"
+    "UNIT", "검정소", "ZD장인지참치",
+    "아들러", "유표", "Voor",
+    "0708ManUtd", "Special블루", "은안",
+    "ZD장인흥미니7", "Angel코레아", "Gucci박장군",
+    "FT난테", "강해린의무회전킥", "kingdom21"
 ]
 
 # 💡 수정된 부분: 2026년 4월 25일 오전 3시 정각
-TARGET_START_DATETIME = "2026-05-18T15:22:03"
+TARGET_START_DATETIME = "2026-05-25T12:00:00"
 
 def get_ouid(nickname):
     url = f"https://open.api.nexon.com/fconline/v1/id?nickname={nickname}"
