3f201d8ec4d2b0259fd91b26c38a0c5231856ace
xldyxl <xldyxl@gmail.com>
2026-06-02 00:50:37 +0900

    Optimize: 00:50:37

--- 올스타리그 경기기록 불러오기/올스타리그경기기록조회.py
+++ 올스타리그 경기기록 불러오기/올스타리그경기기록조회.py
@@ -15,7 +15,7 @@ league_players = [
 ]
 
 # 💡 수정된 부분: 2026년 4월 25일 오전 3시 정각
-TARGET_START_DATETIME = "2026-05-29T14:41:53"
+TARGET_START_DATETIME = "2026-05-30T14:47:49"
 
 def get_ouid(nickname):
     url = f"https://open.api.nexon.com/fconline/v1/id?nickname={nickname}"
